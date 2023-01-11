964b65e333102b1de8e4f54980777c584a2f4490
jainsahab-1506 <2019UCP1390@mnit.ac.in>
2023-01-11 14:13:54 +0530

    Added Loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    "& > * + *": {
+      marginLeft: theme.spacing(2),
+    },
+  },
+  rootBackground: {
+    position: "fixed",
+    width: "100vw",
+    height: "100vh",
+    zIndex: 9999,
+    backgroundColor: "white",
+    opacity: 0.8,
+    display: "grid",
+    placeItems: "center",
+  },
+}));
+
+export default function Loader() {
+  const classes = useStyles();
+
+  return (
+    <>
+      <div className={classes.rootBackground}>
+        <div className={classes.root}>
+          <CircularProgress color="secondary" />
+        </div>
+      </div>
+    </>
+  );
+}
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./home.css";
 import landing from "../../assets/landing.png";
 import MLogo from "../../assets/Mlogo.png";
@@ -10,43 +10,59 @@ import line from "../../assets/line.png";
 import Menu from "../menu/menu";
 import Navbar from "../../common/navbar/navbar";
 import anim from "../../assets/anim1.gif";
+import Loader from "../Loader";
 
 function Home() {
-  return (
-    <div className="back">
-      <div className="videoTag">
-        <img className="videoTag" src={anim} alt="my-gif" />
-      </div>
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
 
-      <img className="landing" src={landing} alt=""></img>
-      <>
-        {/* <div className="smoke_grp">
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="back">
+          <div className="videoTag">
+            <img className="videoTag" src={anim} alt="my-gif" />
+          </div>
+
+          <img className="landing" src={landing} alt=""></img>
+          <>
+            {/* <div className="smoke_grp">
           <img className="smoke" src={smoke} alt=""></img>
           <img className="smoke2" src={smoke2} alt=""></img>
           <img className="smoke3" src={smoke3} alt=""></img>
         </div> */}
-        <div className="line">
-          <img className="line1 " src={line} alt=""></img>
-          <img className="line2" src={line} alt=""></img>
-          <img className="line3" src={line} alt=""></img>
-          <img className="line4" src={line} alt=""></img>
+            <div className="line">
+              <img className="line1 " src={line} alt=""></img>
+              <img className="line2" src={line} alt=""></img>
+              <img className="line3" src={line} alt=""></img>
+              <img className="line4" src={line} alt=""></img>
+            </div>
+            <Navbar />
+            <div className="main">
+              <img className="Mlogo shadowfilter" src={MLogo} alt=""></img>\
+              <div className="content">
+                {" "}
+                Amidst the dusty deserts of Egypt we found some extraterrestrial
+                elements. To discover join hands with us.{" "}
+              </div>
+              <button className="preReg shadowfilter">
+                {" "}
+                <span className="buttonText"> PRE-REGISTER</span>
+              </button>
+              <div className="clouds"></div>
+            </div>
+          </>
         </div>
-        <Navbar />
-        <div className="main">
-          <img className="Mlogo shadowfilter" src={MLogo} alt=""></img>\
-          <div className="content">
-            {" "}
-            Amidst the dusty deserts of Egypt we found some extraterrestrial
-            elements. To discover join hands with us.{" "}
-          </div>
-          <button className="preReg shadowfilter">
-            {" "}
-            <span className="buttonText"> PRE-REGISTER</span>
-          </button>
-          <div className="clouds"></div>
-        </div>
-      </>
-    </div>
+      )}
+    </>
   );
 }
 
